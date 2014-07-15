6f1127c20862cbbcafbb93079401610a7e0c48e6
Bovard Tiberi <bovard.tiberi@gmail.com>
2014-07-15 11:20:45 -0600

    Finished v1 server endpoints

---
+++ main.py
@@ -0,0 +1,123 @@
+import logging
+import json
+import os
+
+from google.appengine.api import users
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import ndb
+
+
+from lib import bottle
+from lib.bottle import abort, post, get, request, error, debug, redirect, response, static_file
+
+import models
+
+
+@get('/')
+def display_home():
+    return static_file('index.html', root='./')
+
+def auth_required():
+    user = users.get_current_user()
+    logging.debug("Checking auth!")
+    if not user:
+        logging.warning("not auth'd!")
+        redirect(users.create_login_url(request.url))
+
+def clan_admin_required(clan, user):
+    if user not in clan.admins:
+        abort(403, 'not a clan admin')
+
+
+def _safe_get_entity(entity_key, model_type=None):
+    entity = ndb.Key(urlsafe=entity_key).get()
+    if not entity:
+        abort(404, 'entity not found')
+
+    if model_type:
+        if not isinstance(entity, model_type):
+            abort(403, 'not the right type')
+
+    return entity
+
+
+@post('/clan/:clan_name')
+def create_clan(clan_name):
+    auth_required()
+    user = users.get_current_user()
+    logging.info("created a new clan {} with user {}".format(clan_name, user.nickname()))
+    clan = models.Clan(
+        admins=[user],
+        name=clan_name,
+    )
+    clan.put()
+    return models.clan_to_json(clan)
+
+
+@post('/clan/:clan_key/member/:member_name')
+def create_clan_member(clan_key, member_name):
+    auth_required()
+
+    user = users.get_current_user()
+
+    clan = _safe_get_entity(clan_key)
+    clan_admin_required(clan, user)
+
+    member = models.ClanMember(
+        parent=clan,
+        name=member_name
+    )
+    member.put()
+    return models.clan_member_to_json(member)
+
+
+@get('/clan/:clan_key/members/')
+def get_clan_members(clan_key):
+    clan = _safe_get_entity(clan_key)
+    members = models.ClanMember(parent=clan)
+    return [models.clan_member_to_json(member) for member in members]
+
+@get('/member/:member_key')
+def get_member(member_key):
+    member = _safe_get_entity(member_key)
+    return models.clan_member_to_json(member)
+
+
+@get('/clan/:clan_key')
+def get_clan(clan_key):
+    clan = _safe_get_entity(clan_key)
+    return models.clan_to_json(clan)
+
+
+@get('/clan/:clan_key/donation_cycles/')
+def get_clan_donation_cycles(clan_key):
+    clan = _safe_get_entity(clan_key, model_type=models.Clan)
+    cycles = models.DonationCycle(parent=clan)
+    return [models.donation_cycle_to_json(cycle) for cycle in cycles]
+
+@post('/clan/:clan_key/donation_cycle')
+def create_new_donation_cycle(clan_key):
+    clan = _safe_get_entity(clan_key)
+
+    cycle = models.DonationCycle(
+        parent=clan
+    )
+    return models.donation_cycle_to_json(cycle)
+
+
+def main():
+    debug(True)
+    app = bottle.app()
+    run_wsgi_app(app)
+
+
+@error(403)
+def error_403(code):
+    return "not authorized (403)"
+
+@error(404)
+def error_404(code):
+    return "not found! (404)"
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,62 @@
+from google.appengine.ext import ndb
+
+class Clan(ndb.Model):
+    admins = ndb.UserProperty(repeated=True)
+    name = ndb.StringProperty()
+
+
+def clan_to_json(clan):
+    admins = []
+    for admin in clan.admins:
+        admins.append(admin.nickname())
+
+    return {
+        'admins': admins,
+        'key': clan.key.urlsafe(),
+        'name': clan.name
+    }
+
+
+class ClanMember(ndb.Model):
+    # parent Clan
+    joined = ndb.DateProperty(auto_now_add=True)
+    name = ndb.StringProperty()
+
+
+def clan_member_to_json(member):
+    return {
+        'clan_key': member.parent.urlsafe(),
+        'name': member.name,
+        'joined': member.joined,
+        'key': member.key.urlsafe()
+    }
+
+
+class DonationCycle(ndb.Model):
+    # parent Clan
+    date = ndb.DateProperty(auto_now_add=True)
+    total = ndb.IntegerProperty(default=0)
+
+def donation_cycle_to_json(cycle):
+    return {
+        'date': cycle.date,
+        'clan_key': cycle.parent.urlsafe(),
+        'key': cycle.key.urlsafe(),
+        'total': cycle.total_donated
+    }
+
+class Donation(ndb.Model):
+    # parent DonationCycle
+    member = ndb.KeyProperty(kind=ClanMember)
+    donated = ndb.IntegerProperty(default=0)
+    received = ndb.IntegerProperty(default=0)
+
+def donation_to_json(donation):
+    return {
+        'donated': donation.donated,
+        'donation_cycle_key': donation.parent.urlsafe(),
+        'key': donation.key.urlsafe(),
+        'member_key': donation.member.urlsafe(),
+        'received': donation.received
+    }
+
