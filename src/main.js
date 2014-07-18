7d2687312f170550e0c064301da0841c75f2e86b
Bovard Tiberi <bovard.tiberi@gmail.com>
2014-07-18 08:40:24 -0600

    Fixed app.yaml to ingnore node modules, added correct title

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ var MenuItem = require('react-bootstrap').MenuItem;
 var Well = require('react-bootstrap').Well;
 var TabbedArea = require('react-bootstrap').TabbedArea;
 var TabPane = require('react-bootstrap').TabPane;
+var PageHeader = require('react-bootstrap').PageHeader;
 
 
 var ClanBox = require('./ClanBox');
@@ -25,12 +26,19 @@ var App = React.createClass({
     },
     render : function() {
         return (
-            <TabbedArea activeKey={this.state.tab} onSelect={this.handleSelect}>
-                <TabPane key={1} tab="Clan">
-                    <ClanBox />
-                </TabPane>
-                <TabPane key={2} tab="Tab 2">TabPane 2 content</TabPane>
-            </TabbedArea>
+            <div>
+                <PageHeader>
+                    Clash Tracker <small>For all your clan data needs</small>
+                </PageHeader>
+                <TabbedArea activeKey={this.state.tab} onSelect={this.handleSelect}>
+                    <TabPane key={1} tab="Clan">
+                        <Well>
+                            <ClanBox />
+                        </Well>
+                    </TabPane>
+                    <TabPane key={2} tab="Tab 2">TabPane 2 content</TabPane>
+                </TabbedArea>
+            </div>
         );
     }
 });
